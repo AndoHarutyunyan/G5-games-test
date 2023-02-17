e5ffec8e10ab61976268309a5fe5b4a443a8ac41
andoharutyunyan <Andochaxkal1990@gmail.com>
2023-02-17 18:00:01 +0400

    use vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,74 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { fetchDogs, fetchProducts } from "../api/api";
+import { verifyImageFormat } from "../validations/regex";
+import { URLs } from "../api/url";
+import { storageKeys } from "../helpers/storageKeys";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    dogs: [],
+    products: [],
+    singleDogData: null,
+  },
+  getters: {
+    GET_PRODUCTS(state) {
+      return state.products;
+    },
+    GET_SINGLE_DOG_DATA(state) {
+      return state.singleDogData;
+    },
+    GET_DOGS(stete) {
+      return stete.dogs;
+    },
+  },
+  mutations: {
+    SET_DOGS(state, payload) {
+      state.dogs = payload;
+    },
+    SET_PRODUCTS(state, payload) {
+      state.products = payload;
+    },
+    SET_SINGLE_DOG_DATA(state, payload) {
+      state.singleDogData = payload;
+    },
+  },
+
+  actions: {
+    async getDogImgUrl({ state, commit, dispatch }) {
+      try {
+        const result = await fetchDogs(URLs.RANDOM_DOG_URL);
+        if (verifyImageFormat(result.data.url)) {
+          commit("SET_SINGLE_DOG_DATA", null);
+          dispatch("getDogImgUrl");
+        } else {
+          commit("SET_SINGLE_DOG_DATA", result.data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async getProducts({ state, commit }) {
+      try {
+        if (localStorage.getItem(storageKeys.localStorage.PRODUCTS)) {
+          commit(
+            "SET_PRODUCTS",
+            JSON.parse(localStorage.getItem(storageKeys.localStorage.PRODUCTS))
+          );
+        } else {
+          const result = await fetchProducts(URLs.PRODUCTS_URL);
+          commit("SET_PRODUCTS", result.data.products);
+          localStorage.setItem(
+            storageKeys.localStorage.PRODUCTS,
+            JSON.stringify(result.data.products.slice(0, 20))
+          );
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
   modules: {},
 });
--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <template v-if="!dogs.length">
+    <template v-if="!GET_DOGS.length">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -30,11 +30,11 @@
       </svg>
     </template>
     <template v-else>
-      <GeneralSlider :sliderData="dogs" :sliderOptions="dogSliderConfig.dog" />
+      <GeneralSlider :sliderData="GET_DOGS" :sliderOptions="dogSliderConfig.dog" />
     </template>
     <div class="product_contener">
       <ProductCard
-        v-for="(item, index) in products"
+        v-for="(item, index) in GET_PRODUCTS"
         :key="item.id"
         :product="item"
         :topSalePopular="isTopSale(index)"
@@ -48,10 +48,8 @@ import GeneralSlider from "../components/elements/GeneralSlider.vue";
 import ProductCard from "../components/elements/ProductCard.vue";
 import sliderConfig from "../config/slider";
 import { getCookie, setCookie } from "../helpers/cookie";
-import { verifyImageFormat } from "../validations/regex";
 import { storageKeys } from "../helpers/storageKeys";
-import { URLs } from "../api/url";
-import { fetchDogs, fetchProducts } from "../api/api";
+import { mapActions, mapGetters, mapMutations } from "vuex";
 
 export default {
   name: "gallary-view",
@@ -62,67 +60,30 @@ export default {
 
   data() {
     return {
-      dogs: [],
       dogSliderConfig: sliderConfig,
-      singleDogData: null,
-      products: [],
     };
   },
   methods: {
-    setCookie,
-    getCookie,
-    verifyImageFormat,
-    fetchDogs,
-    fetchProducts,
-    async getDogImgUrl() {
-      try {
-        const result = await this.fetchDogs(URLs.RANDOM_DOG_URL);
-        if (this.verifyImageFormat(result.data.url)) {
-          this.singleDogData = null;
-          this.getDogImgUrl();
-        } else {
-          this.singleDogData = result.data;
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    async getProducts() {
-      try {
-        if (localStorage.getItem(storageKeys.localStorage.PRODUCTS)) {
-          this.products = JSON.parse(
-            localStorage.getItem(storageKeys.localStorage.PRODUCTS)
-          );
-        } else {
-          const result = await this.fetchProducts(URLs.PRODUCTS_URL);
-          this.products = result.data.products;
-          localStorage.setItem(
-            storageKeys.localStorage.PRODUCTS,
-            JSON.stringify(result.data.products.slice(0, 20))
-          );
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    },
+    ...mapMutations(["SET_DOGS"]),
+    ...mapActions(["getDogImgUrl", "getProducts"]),
+
     getDogsData() {
-      if (!this.getCookie(storageKeys.cookies.DOGS)) {
+      if (!getCookie(storageKeys.cookies.DOGS)) {
         this.updateDogs();
       } else {
-        this.dogs = JSON.parse(this.getCookie(storageKeys.cookies.DOGS));
+        this.SET_DOGS(JSON.parse(getCookie(storageKeys.cookies.DOGS)));
       }
     },
     async updateDogs() {
       const dogsArr = [];
       while (dogsArr.length < 10) {
         await this.getDogImgUrl();
-        if (this.singleDogData) {
-          dogsArr.push(this.singleDogData);
-          this.counter++;
+        if (this.GET_SINGLE_DOG_DATA) {
+          dogsArr.push(this.GET_SINGLE_DOG_DATA);
         }
       }
-      this.dogs = dogsArr;
-      this.setCookie(
+      this.SET_DOGS(dogsArr);
+      setCookie(
         storageKeys.cookies.DOGS,
         JSON.stringify(this.dogs),
         5 /*Minutes*/
@@ -131,6 +92,9 @@ export default {
     isTopSale(index) {
       return index < 3;
     },
+  },
+  computed: {
+    ...mapGetters(["GET_PRODUCTS", "GET_SINGLE_DOG_DATA", "GET_DOGS"]),
   },
   created() {
     this.getDogsData();
